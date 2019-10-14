eb4ad15d20984fa4432f112b4ca9094f12e0cc34
DTaraska11 <56418437+DTaraska11@users.noreply.github.com>
2019-10-14 02:09:11 -0400

    Update myshell.h

--- myshell.h
+++ myshell.h
@@ -21,27 +21,25 @@
 /*GLOBAL VARIABLES*/
 int pipe_count=0, fd;
 static char* args[512];
-char *history_file; ///
 char input_buff[1024];
 char *exec_cmd[100];
 int flag, len;
-char cwd[1024]; ///
 int flag_pipe=1;
 pid_t pid;
 int num_Lines;
 int envFlag;
-int flag_pipe, flag_without_pipe,  output_redirection, input_redirection; ///
+
 int bangFlag;
 int pid, status;
-char history_data[1000][1000]; ///
+
 char curr_Dir[1000];
-char ret_file[3000]; ///
-char his_var[2000]; ///
+
+
 char *input_rediFile;
 char *output_rediFile;
-extern char** environ; ///
 
-/***************************Header Files Used*****************************/
+
+
 void clr_var();
 void file_Procs ();
 void file_Write();
@@ -51,7 +49,7 @@ void set_EnvVariables();
 void change_directory();
 void parent_directory();
 void echo_internal(char *echo_val);
-void history_execute_with_constants(); ///
+
 static char* ignore_whiteSpace(char* s);
 void tok_cmds(char *com_exec);
 void tok_rediIO(char *exec_cmd);
@@ -64,4 +62,3 @@ static int command(int, int, int, char *exec_cmd);
 void prompt();
 void sig_int(int sig_num);
 
-/*************************************************************************/
